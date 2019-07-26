55ba5c7fe26c16986b53b063e791f607e0a16a07
Aman <rumman.djuice1@gmail.com>
2019-07-26 20:51:08 +0200

    "add manual pre-setups for jest in the setupTests.js"

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 
 module.exports={
+    setupTestFrameworkScriptFile:'<rootDir>/test/setupTests.js',//so we can add a default testSetup File
     moduleNameMapper: {
       "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
       "\\.(css|less)$": "identity-obj-proxy"
